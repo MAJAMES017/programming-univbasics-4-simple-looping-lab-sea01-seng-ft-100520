70926e4e619f10000801c5377c71290bb39ba435
michael james <majames071@gmail.com>
2020-09-14 02:36:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,7 +5,7 @@ def loop_message_five_times(message)
     puts message
     count += 1
   end
-end  
+end
 
 def loop_message_n_times(message, limit)
   count = 0
@@ -25,7 +25,7 @@ end
 
 def return_string_array(array)
   count = 0
-  while  count < array.lenght do 
+  while  count < array.lenght do
     count += 1
-  end  
+  end
 end
